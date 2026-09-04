885602cd572bf8ab3321112a8369d21db9193bfb
Philipe <norphilipe@gmail.com>
2026-09-04 09:46:08 -0300

    fix: use official vinext Worker handler signature

--- worker/index.ts
+++ worker/index.ts
@@ -1,16 +1,7 @@
 import handler from "vinext/server/app-router-entry";
 
-interface Env {
-  ASSETS: Fetcher;
-  IMAGES?: unknown;
-}
-
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
-    return handler.fetch(request, env, ctx);
+  async fetch(request: Request): Promise<Response> {
+    return handler.fetch(request);
   },
 };
